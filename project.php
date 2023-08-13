b8a59eb8cc790cd88ec4236146791053e941bd87
indev-aj <amrin.wan@gmail.com>
2023-08-13 13:59:09 +0100

    Added project page

--- project.php
+++ project.php
@@ -14,8 +14,66 @@
 
     <div class="container">
 
-        <?php include_once('header.php') ?>
-        <?php include_once('./dev/wip.php') ?>
+        <?php include_once('./header.php')   ?>
+
+        <div class="project-section">
+            <div class="project-section-title">Projects</div>
+            <div class="projects">
+            <div class="project">
+                    <div class="project-thumbnail">
+                        <img src="./images/imswitch.jpg" alt="">
+                    </div>    
+                    <div class="project-title">ImSwitch</div>
+                    <div class="project-subtitle">4th year project</div>
+                    <div class="project-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum delectus, fugiat vel corporis reprehenderit quos quaerat, numquam distinctio deserunt nulla voluptatibus pariatur ipsum.</div>
+                </div>
+
+                <div class="project">
+                    <div class="project-thumbnail">
+                        <img src="./images/binary.jpg" alt="">
+                    </div>    
+                    <div class="project-title">ImSwitch</div>
+                    <div class="project-subtitle">4th year project</div>
+                    <div class="project-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum delectus, fugiat vel corporis reprehenderit quos quaerat, numquam distinctio deserunt nulla voluptatibus pariatur ipsum.</div>
+                </div>
+
+                <div class="project">
+                    <div class="project-thumbnail">
+                        <img src="./images/imswitch.jpg" alt="">
+                    </div>    
+                    <div class="project-title">ImSwitch</div>
+                    <div class="project-subtitle">4th year project</div>
+                    <div class="project-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum delectus, fugiat vel corporis reprehenderit quos quaerat, numquam distinctio deserunt nulla voluptatibus pariatur ipsum.</div>
+                </div>
+
+                <div class="project">
+                    <div class="project-thumbnail">
+                        <img src="./images/binary.jpg" alt="">
+                    </div>    
+                    <div class="project-title">ImSwitch</div>
+                    <div class="project-subtitle">4th year project</div>
+                    <div class="project-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum delectus, fugiat vel corporis reprehenderit quos quaerat, numquam distinctio deserunt nulla voluptatibus pariatur ipsum.</div>
+                </div>
+
+                <div class="project">
+                    <div class="project-thumbnail">
+                        <img src="./images/imswitch.jpg" alt="">
+                    </div>    
+                    <div class="project-title">ImSwitch</div>
+                    <div class="project-subtitle">4th year project</div>
+                    <div class="project-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum delectus, fugiat vel corporis reprehenderit quos quaerat, numquam distinctio deserunt nulla voluptatibus pariatur ipsum.</div>
+                </div>
+
+                <div class="project">
+                    <div class="project-thumbnail">
+                    <img src="./images/binary.jpg" alt="">
+                    </div>    
+                    <div class="project-title">ImSwitch</div>
+                    <div class="project-subtitle">4th year project</div>
+                    <div class="project-summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate earum delectus, fugiat vel corporis reprehenderit quos quaerat, numquam distinctio deserunt nulla voluptatibus pariatur ipsum.</div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </body>
