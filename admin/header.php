3a98a2cc9e62bdedd45befb61730692f6ea9fc89
indev-aj <amrin.wan@gmail.com>
2023-10-09 12:28:50 +0100

    Fixed admin header hamburger menu in mobile

--- admin/header.php
+++ admin/header.php
@@ -5,7 +5,7 @@ include("./service/auth_session.php");
 ?>
 
 
-<!-- Navbar -->
+<!-- Navbar
 <div class="header-container">
     <div class="logo"><a href="./bio.php">INDEV</a></div>
     <div class="navbar">
@@ -19,13 +19,31 @@ include("./service/auth_session.php");
         </ul>
     </div>
 
-    <div class="hamburger" onclick="menu()" style="display: none;">
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
+    <div class="hamburger" onclick="menu()">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+    </div>
+</div> -->
+
+<div class="header-container">
+    <div class="logo"><a href="./bio.php">INDEV</a></div>
+    <div class="navbar">
+        <ul class="nav-list">
+            <li><a href="./project.php">Project</a></li>
+            <li><a href="./experience.php">Experience</a></li>
+            <li><a href="./award.php">Award</a></li>
+            <li><a href="./skill.php">Skill</a></li>
+            <li><a href="./bio.php">Bio</a></li>
+            <li><a href="./service/logout.php">Logout</a></li>
+        </ul>
+        <div class="hamburger" onclick="menu()">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
     </div>
 </div>
-
 
 <script src="./script.js"></script>
 <script src="../script.js"></script>
