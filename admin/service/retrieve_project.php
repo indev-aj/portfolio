9440cb561f6891482be3a603283ffa2bca39f168
indev-aj <amrin.wan@gmail.com>
2023-10-16 22:47:37 +0100

    Fixed data retrieval UI

--- admin/service/retrieve_project.php
+++ admin/service/retrieve_project.php
@@ -8,7 +8,12 @@ $user_id = $_SESSION['user']['id'];
 $sql = "SELECT * FROM Project where user_id='$user_id'";
 $result = $conn->query($sql);
 $user;
+?>
 
+<div class="project-section">
+<div class="projects">
+
+<?php
 if ($result->num_rows > 0) {
 
     while ($user = $result->fetch_assoc()) {
@@ -19,20 +24,18 @@ if ($result->num_rows > 0) {
         $thumbnail = "./" . $thumbnail;
         $thumbnail = str_replace(' ', '%20', $thumbnail);
 ?>
-
-
-        <div class="projects">
-            <div class="card">
-                <div class="project">
+            <div class="card card-project">
+                <div class="project admin-project">
                     <div class="project-thumbnail">
-                        <img src=<?= $thumbnail ?> alt="project image">
+                        <img src=<?= $thumbnail ?> alt="project image" loading="lazy">
                     </div>
                     <div class="project-title"><?= $title ?></div>
                     <div class="project-subtitle"><?= $subtitle ?></div>
                     <div class="project-summary"><?= $summary ?></div>
                 </div>
             </div>
-        </div>
 
 <? }
-}
+} ?>
+</div>
+</div>
