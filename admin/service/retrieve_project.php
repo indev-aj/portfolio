e82199fc2036a5d1cf0679fa10fc6b37687da2e9
indev-aj <amrin.wan@gmail.com>
2023-10-18 11:06:16 +0100

    fix styling on admin project

--- admin/service/retrieve_project.php
+++ admin/service/retrieve_project.php
@@ -11,31 +11,44 @@ $user;
 ?>
 
 <div class="project-section">
-<div class="projects">
+    <div class="projects">
 
-<?php
-if ($result->num_rows > 0) {
+        <?php
+        if ($result->num_rows > 0) {
 
-    while ($user = $result->fetch_assoc()) {
-        $title =  $user['title'];
-        $subtitle = $user['subtitle'];
-        $summary =  $user['summary'];
-        $thumbnail = substr($user['thumbnail'], 2);
-        $thumbnail = "./" . $thumbnail;
-        $thumbnail = str_replace(' ', '%20', $thumbnail);
-?>
-            <div class="card card-project">
-                <div class="project admin-project">
-                    <div class="project-thumbnail">
-                        <img src=<?= $thumbnail ?> alt="project image" loading="lazy">
+            while ($user = $result->fetch_assoc()) {
+                $title =  $user['title'];
+                $subtitle = $user['subtitle'];
+                $summary =  $user['summary'];
+                $thumbnail = substr($user['thumbnail'], 2);
+                $thumbnail = "./" . $thumbnail;
+                $thumbnail = str_replace(' ', '%20', $thumbnail);
+        ?>
+                <div class="card card-project">
+                    <div class="project admin-project">
+                        <div>
+                            <div class="project-thumbnail">
+                                <img src=<?= $thumbnail ?> alt="project image" loading="lazy">
+                            </div>
+                            <div class="project-title"><?= $title ?></div>
+                            <div class="project-subtitle"><?= $subtitle ?></div>
+                            <div class="project-summary"><?= $summary ?></div>
+                        </div>
+
+                        <form action="./retrieve_project.php" method="get" class="edit-form">
+                            <div class="column">
+                                <div class="col-item">
+                                    <input type="button" value="edit" class="edit-project-btn">
+                                </div>
+                                <div class="col-item">
+                                    <input type="button" value="delete" class="delete-project-btn">
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="project-title"><?= $title ?></div>
-                    <div class="project-subtitle"><?= $subtitle ?></div>
-                    <div class="project-summary"><?= $summary ?></div>
                 </div>
-            </div>
 
-<? }
-} ?>
-</div>
+        <? }
+        } ?>
+    </div>
 </div>
